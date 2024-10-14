81b3b81b34d9bb21c1d62c8d1790da9834702d9c
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-10-14 09:52:58 +0300

    Added Reflection and Attributes solutions
    Added solutions of the exercises from the Reflection and Attributes lesson

---
+++ C#OOP-Feb2023/ReflectionAndAttributes/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,35 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] arguments = args
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string commandType = arguments[0];
+            string[] commandArguments = arguments.Skip(1).ToArray();
+
+            string commandName = $"{commandType}Command";
+
+            Type type = Assembly
+                .GetEntryAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == commandName);
+
+            if (type == null )
+            {
+                throw new ArgumentException("Invalid command type");
+            }
+
+            ICommand command = Activator.CreateInstance(type) as ICommand;
+
+            return command.Execute(commandArguments);
+        }
+    }
+}
---
+++ C#OOP-Feb2023/ReflectionAndAttributes/CommandPattern/Core/Engine.cs
@@ -0,0 +1,37 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter interpreter;
+
+        public Engine(ICommandInterpreter interpreter)
+        {
+            this.interpreter = interpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string arguments = Console.ReadLine();
+
+                try
+                {
+                    string result = interpreter.Read(arguments);
+                    Console.WriteLine(result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
